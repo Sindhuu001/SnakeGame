645908f2e40341c94b2d975399e7188d24730b57
SINDHU <SINDHU@LAPTOP-MC0C48PD>
2024-10-16 14:04:33 +0530

    first commit

--- src/main/GameFrame.java
+++ src/main/GameFrame.java
@@ -7,6 +7,11 @@ import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public static void Frame() {
 		GameFrame gm = new GameFrame();
 		SnakePanel gp = new SnakePanel();
